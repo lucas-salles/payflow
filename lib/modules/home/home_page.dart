463c9ef19af070df943010858a8f925a8b18423a
Lucas Sales <lucas@yupistudios.com>
2021-10-07 17:02:52 -0300

    Adicionando BoletoTileWidget

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/modules/home/home_controller.dart';
+import 'package:payflow/shared/models/boleto_model.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/boleto_tile/boleto_tile_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,7 +16,12 @@ class _HomePageState extends State<HomePage> {
   final controller = HomeController();
   final pages = [
     Container(
-      color: Colors.red,
+      child: BoletoTileWidget(
+          data: BoletoModel(
+              name: "Teste",
+              dueDate: "07/10/2021",
+              value: 100,
+              barcode: "16asdfsd211")),
     ),
     Container(
       color: Colors.blue,

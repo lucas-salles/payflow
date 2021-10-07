07247e4ccabd1f6cd0fb1e02abd1069d83f43d37
Lucas Sales <lucas@yupistudios.com>
2021-10-07 18:08:56 -0300

    Adicionando ExtractPage

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/extract/extract_page.dart';
 import 'package:payflow/modules/home/home_controller.dart';
 import 'package:payflow/modules/meus_boletos/meus_boletos_page.dart';
-import 'package:payflow/shared/models/boleto_model.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
-import 'package:payflow/shared/widgets/boleto_list/boleto_list_widget.dart';
-import 'package:payflow/shared/widgets/boleto_tile/boleto_tile_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -18,9 +16,7 @@ class _HomePageState extends State<HomePage> {
   final controller = HomeController();
   final pages = [
     MeusBoletosPage(),
-    Container(
-      color: Colors.blue,
-    )
+    ExtractPage(),
   ];
   @override
   Widget build(BuildContext context) {
@@ -70,7 +66,9 @@ class _HomePageState extends State<HomePage> {
                 },
                 icon: Icon(
                   Icons.home,
-                  color: AppColors.primary,
+                  color: controller.currentPage == 0
+                      ? AppColors.primary
+                      : AppColors.body,
                 )),
             GestureDetector(
               onTap: () {
@@ -96,7 +94,9 @@ class _HomePageState extends State<HomePage> {
                 },
                 icon: Icon(
                   Icons.description_outlined,
-                  color: AppColors.body,
+                  color: controller.currentPage == 1
+                      ? AppColors.primary
+                      : AppColors.body,
                 )),
           ],
         ),
